4a08b6c8c0567451531ea68b15e050179a530c64
Kunanont <149161980+Troxq@users.noreply.github.com>
2024-04-21 09:32:29 +0000

    Log_add/Log_modified

---
+++ Logmain.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "Log.h"
+
+void Logmenu()
+{
+    int choice=0;
+    system("clear");
+    while(true)
+    {
+        try
+        {
+            cout << "We track all of your progress" << endl;
+            cout << "What progress you want to see" << endl;
+            cout << "[1] Register" << endl;
+            cout << "[2] Login" << endl;
+            cout << "[3] import" << endl;
+            cout << "[4] export" << endl;
+            cout << "[5] sent_order" << endl;
+            cout << "[6] confirm_order" << endl;
+            cout << "[7] transfer" << endl;
+            cout << "Input your choice : " << endl;
+            cin >> choice;
+
+            if(cin.fail() || choice > 7 || choice < 1)
+            {
+                throw 1;
+            }
+
+            break;
+        }
+
+        catch(...)
+        {
+            cout << "Choice not available" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+    }
+
+    if(choice == 1)
+    {
+        system("clear");
+        regis_show();
+    }
+
+    else if(choice == 2)
+    {
+        system("clear");
+        login_show();
+    }
+}   
